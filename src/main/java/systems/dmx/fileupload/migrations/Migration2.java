dbcb57364681da3d7219bf206e7aee1b9275ce79
Jörg Richter <jri@deepamehta.de>
2021-08-25 00:43:52 +0200

    Migr. 2: remove duplicate "Configuration" assocs (#5)

---
+++ src/main/java/systems/dmx/fileupload/migrations/Migration2.java
@@ -0,0 +1,37 @@
+package systems.dmx.fileupload.migrations;
+
+import static systems.dmx.accesscontrol.Constants.*;
+import static systems.dmx.config.Constants.*;
+import static systems.dmx.core.Constants.*;
+import static systems.dmx.files.Constants.*;
+
+import systems.dmx.core.RelatedTopic;
+import systems.dmx.core.Topic;
+import systems.dmx.core.service.Migration;
+
+import java.util.List;
+
+
+
+/**
+ * Removes duplicate "Configuration" assocs.
+ * <p>
+ * Part of DMX File Upload 1.0.4
+ * Runs ALWAYS
+ */
+public class Migration2 extends Migration {
+
+    // -------------------------------------------------------------------------------------------------- Public Methods
+
+    @Override
+    public void run() {
+        for (Topic username : dmx.getTopicsByType(USERNAME)) {
+            List<RelatedTopic> topics = username.getRelatedTopics(CONFIGURATION, CONFIGURABLE, DEFAULT, DISK_QUOTA);
+            if (topics.size() > 1) {
+                for (int i = 1; i < topics.size(); i++) {
+                    topics.get(i).getRelatingAssoc().delete();
+                }
+            }
+        }
+    }
+}
